3059c2b39ec36632bba8eb65160605d78f246220
Desarrollador de Software <estiven.perea@CARDINAL.LOCAL>
2021-02-15 11:52:57 -0500

    subiendo archivos

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import Pokedex from './Pokedex';
+import Pokemon from "./Pokemon";
+
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" render={(props) => <Pokedex {...props} />} />
+    <Route
+      exact
+      path="/:pokemonId"
+      render={(props) => <Pokemon {...props} />}
+    />
+  </Switch>
+);
+
+export default App;
